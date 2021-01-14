b3e0c56c512194d0443c8bbce892f7fcf550b586
Lenard Okorn <lenardokorn@gmail.com>
2021-01-14 08:42:12 +0100

    Added all the current website files (HTML, CSS, JS, images...)

---
+++ js/scroll-to-anchor.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$( "a.scrollLink" ).click(function( event ) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+	});
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+	event.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 500);
+});
