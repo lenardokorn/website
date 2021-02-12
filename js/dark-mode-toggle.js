d5c6bc3e0c84a66ac9f893d25966bbb1add576f5
Lenard Okorn <lenardokorn@gmail.com>
2021-02-12 20:04:33 +0100

    Added footer and landing page content

--- js/dark-mode-toggle.js
+++ js/dark-mode-toggle.js
@@ -1,8 +1,21 @@
 const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {
+    // Single elements
     document.body.classList.toggle('dark');
     document.querySelector('.navbar').classList.toggle('navbar-dark');
     document.querySelector('.navbar').classList.toggle('bg-dark');
     document.querySelector('#projects-dropdown').classList.toggle('dropdown-menu-dark');
+    document.querySelector('.landing-text').classList.toggle('landing-text-dark');
+    document.querySelector('.footer').classList.toggle('bg-dark');
+
+    // Multiple elements
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].classList.toggle('bg-dark');
+    }
+    let buttons = document.getElementsByClassName('btn');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.toggle('btn-dark');
+    }
 });
