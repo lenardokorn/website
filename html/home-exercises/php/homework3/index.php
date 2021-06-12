cfaebdb05fed49883a88e17e3bf70eab6f219ca3
Lenard Okorn <lenardokorn@gmail.com>
2021-06-12 13:39:24 +0200

    Added PHP Homework 3

--- html/home-exercises/php/homework3/index.php
+++ html/home-exercises/php/homework3/index.php
@@ -6,16 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Homework 3</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <?php
+    function counter()
+    {
+        $fileName = "counter.txt";
+        $count = 0;
+        $handle = fopen($fileName, "r+");
+        flock($handle, LOCK_EX);
+        $count = fgets($handle, 10);
+        fseek($handle, 0);
+        echo "" . ++$count;
+        fputs($handle, $count);
+        fclose($handle);
+    }
+    ?>
+
     <main class="container mt-5">
         <h1>PHP Homework 3</h1>
         <p class="lead">
-            By Nina Moritz and Lenard Okorn
+            By Nina Moritz and Lenard Okorn <span class="badge bg-dark">
+                <?php counter(); ?> Visits
+            </span>
         </p>
 
         <form action="action.php" method="GET">
@@ -36,9 +52,7 @@
         </form>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
