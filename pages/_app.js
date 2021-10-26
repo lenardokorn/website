bc74b19acd5be3f7d3133b0274bf01a08e2ffd21
Lenard Okorn <lenardokorn@gmail.com>
2021-10-26 21:00:27 +0200

    Set up Tailwind CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+// TODO: Figure out how to use global.css instead of this import:
+import 'tailwindcss/tailwind.css';
 import Layout from '../components/layouts/layout';
 
 export default function App({ Component, pageProps, router }) {
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello World!</div>;
+  return <p className="text-center font-bold text-blue-700">Hello World!</p>;
 }
