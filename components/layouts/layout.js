27befed168c2c21114cb8a0d029033d49e10aa19
Lenard Okorn <lenardokorn@gmail.com>
2021-10-27 23:18:25 +0200

    Remove unnecessary <div>

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 export default function Layout({ children, router }) {
   return (
-    <div>
+    <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Lenard Okorn's personal website" />
         <title>Lenard Okorn</title>
       </Head>
       <main>{children}</main>
-    </div>
+    </>
   );
 }
