f01e73ab6bf94d5b136d8179e2b616737a6def7c
Lenard Okorn <lenardokorn@gmail.com>
2021-10-28 16:53:19 +0200

    Add images from old website

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -5,7 +5,6 @@ export default function Layout({ children, router }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Lenard Okorn's personal website" />
         <title>Lenard Okorn</title>
       </Head>
       <main>{children}</main>
--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="description" content="Lenard Okorn's personal website" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
