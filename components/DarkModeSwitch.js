df38d219175b6b48ab02bcc48513a323c5d9c84a
Lenard Okorn <lenardokorn@gmail.com>
2021-11-12 19:28:56 +0100

    Edit dark mode switch styling

--- components/DarkModeSwitch.js
+++ components/DarkModeSwitch.js
@@ -13,7 +13,7 @@ export default function DarkModeSwitch() {
         className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md cursor-pointer"
         onChange={handleChange}
       />
-      <span className="w-14 h-7 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-6 after:h-6 after:bg-white after:rounded-full after:duration-300 peer-checked:after:translate-x-6"></span>
+      <span className="w-14 h-7 flex items-center flex-shrink-0 ml-4 p-1 bg-gradient-to-r shadow-sm rounded-full duration-300 ease-in-out from-pink-600 via-yellow-500 to-yellow-400 peer-checked:from-blue-600 peer-checked:via-indigo-600 peer-checked:to-purple-800  after:w-6 after:h-6 after:bg-white after:shadow-sm after:rounded-full after:duration-300 peer-checked:after:translate-x-6"></span>
     </label>
   );
 }
