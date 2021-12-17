4eb9087907a363e842b3da7380ccc0afb89a710e
Lenard Okorn <lenardokorn@gmail.com>
2021-12-17 17:40:03 +0100

    Style personal project posts

--- pages/personal-projects/[id].jsx
+++ pages/personal-projects/[id].jsx
@@ -1,27 +1,32 @@
 import { getAllPostsIds, getPostData } from '../../lib/posts';
-import Link from 'next/link';
 import { BsGithub } from 'react-icons/bs';
+import LinkButton from '../../components/link-button';
 
 export default function Post({ postData }) {
   return (
     <>
-      <Link href="/personal-projects">
-        <a>←Back</a>
-      </Link>
-      <br />
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      {postData.git !== undefined && (
-        <Link href={`https://github.com/${postData.git}`}>
-          <a target="_blank">
-            <BsGithub className="inline-block" /> View on GitHub
-          </a>
-        </Link>
-      )}
+      <section>
+        <div className="flex">
+          <LinkButton href="/personal-projects">← Back</LinkButton>
+          {/* <Link href="/personal-projects">
+            <a>← Back</a>
+          </Link> */}
+          {postData.git !== undefined && (
+            <div className="ml-auto">
+              <LinkButton href={`https://github.com/${postData.git}`}>
+                <BsGithub className="inline-block" /> View on GitHub
+              </LinkButton>
+            </div>
+          )}
+        </div>
+        <h1 className="mt-3 mb-1">{postData.title}</h1>
+        <span className="bg-lighter dark:bg-darker py-1 px-3 rounded-full">
+          {postData.date}
+        </span>
+      </section>
+
+      <hr />
+
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </>
   );
