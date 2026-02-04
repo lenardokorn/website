c27eadf9e51b1a7abd8abbe5bd1c8f50a7faadbe
Lenard Okorn <lenardokorn@gmail.com>
2026-02-04 13:38:40 +0100

    Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-
 import solidJs from '@astrojs/solid-js';
 import node from '@astrojs/node';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lenardokorn.com',
-  integrations: [mdx(), sitemap(), solidJs()],
+  integrations: [mdx(), sitemap(), solidJs(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'static',
   adapter: node({ mode: 'standalone' }),
 });
